bac0afdaa54e434e262cc8171aa187ff8d0efcce
DayDream-21 <vova13158@gmail.com>
2023-03-29 20:54:09 +0300

    Configure auth with KeyCloak token

---
+++ discovery-server/src/main/java/com/slavamashkov/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.slavamashkov.discoveryserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return httpSecurity.build();
+    }
+}
